c3283619d8004d5923ce47a4ec5baf9633eec033
Abhishek Sharma <abhishm20@gmail.com>
2023-06-07 15:11:24 +0530

    multiple container issue fixes

--- ecs_task_def_json.py
+++ ecs_task_def_json.py
@@ -3,20 +3,22 @@ import sys
 import os
 
 
-def create_task_definition(json_file, image_name):
+def create_task_definition(json_file, container_images):
     """Add image and environment in json file
 
     Args:
         json_file (String): File path
-        containers (List): Containers with container_name and image
+        container_images (List): Containers with container_name and image_name
         envs (List): List of object with name and value
     """
     envs = json.loads(os.getenv('TASK_ENVS', "{}"))
     with open(json_file, "r", encoding="utf-8") as file_pointer:
         task_json = json.load(file_pointer)
     for container_def in task_json["taskDefinition"]["containerDefinitions"]:
-        container_def["image"] = image_name
-        container_def["environment"] = envs
+        for con_img in json.loads(container_images):
+            if con_img["container_name"] == container_def['name']:
+                container_def["image"] = con_img["image_name"]
+                container_def["environment"] = envs
     with open(json_file, "w", encoding="utf-8") as file_pointer:
         json.dump(task_json, file_pointer)
 
