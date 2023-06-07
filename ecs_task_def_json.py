6ae73deafa5dd1e3b54091aca1e298ce7bbfb607
Abhishek Sharma <abhishm20@gmail.com>
2023-06-07 18:06:48 +0530

    multiple container issue fixes

--- ecs_task_def_json.py
+++ ecs_task_def_json.py
@@ -3,25 +3,26 @@ import sys
 import os
 
 
-def create_task_definition(json_file, container_images):
+def create_task_definition(json_file, container_names, images_names):
     """Add image and environment in json file
 
     Args:
         json_file (String): File path
         container_images (List): Containers with container_name and image_name
     """
-    container_images = json.loads(container_images)
+    container_names = container_names.split(",")
+    images_names = images_names.split(",")
     envs = json.loads(os.getenv('TASK_ENVS', "{}"))
     with open(json_file, "r", encoding="utf-8") as file_pointer:
         task_json = json.load(file_pointer)
     for container_def in task_json["taskDefinition"]["containerDefinitions"]:
-        for con_img in container_images:
-            if con_img["container_name"] == container_def['name']:
-                container_def["image"] = con_img["image_name"]
+        for index, container_name in enumerate(container_names):
+            if container_name == container_def['name']:
+                container_def["image"] = images_names[index]
                 container_def["environment"] = envs
     with open(json_file, "w", encoding="utf-8") as file_pointer:
         json.dump(task_json, file_pointer)
 
 
 if __name__ == "__main__":
-    create_task_definition(sys.argv[1], sys.argv[2])
+    create_task_definition(sys.argv[1], sys.argv[2], sys.argv[3])
